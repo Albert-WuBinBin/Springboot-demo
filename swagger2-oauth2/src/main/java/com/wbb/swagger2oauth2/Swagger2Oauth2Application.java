6c89a7897af5e7c9e4f874cba2428e42de595a69
e <469548738@qq.com>
2019-01-17 22:51:17 +0800

    modify swagger

---
+++ swagger2-oauth2/src/main/java/com/wbb/swagger2oauth2/Swagger2Oauth2Application.java
@@ -0,0 +1,14 @@
+package com.wbb.swagger2oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2Oauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2Oauth2Application.class, args);
+	}
+
+}
+
